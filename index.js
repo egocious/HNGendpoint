72781ecaf52edfba25b7bfa672d57edbc917ff0e
Ego.Ugwu <egougwu11@yahoo.com>
2022-10-29 00:08:46 +0100

    changed port

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-var PORT =process.env.PORT ||8888;
+//const PORT =process.env.PORT  || 8888;
 const express= require('express')
 const data = require('./server')
 
 const api = express()
 
 const HOST ='localhost'
+const PORT =8888
 
 api.get('/',(req,res) =>{
-    res.send('welcom')
+    res.status(200).json(data)
       })
 
-
-api.get('/details',(req,res) =>{
-    res.status(200).json(data)
-})
-
-api.listen(PORT, () => console.log('API running at ${HOST}:${PORT}'))
+api.listen(PORT, () => console.log('API running at ${HOST}:${PORT}'));

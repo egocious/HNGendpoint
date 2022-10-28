9a308f2b9889e57589ee6b24240c30e047f5a66d
Ego.Ugwu <egougwu11@yahoo.com>
2022-10-28 20:17:53 +0100

    update to port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+var PORT =process.env.PORT ||8888;
 const express= require('express')
-const data =require('./server')
+const data = require('./server')
 
 const api = express()
 
 const HOST ='localhost'
-const PORT = 8888
 
 api.get('/',(req,res) =>{
     res.send('welcom')
@@ -14,5 +14,5 @@ api.get('/',(req,res) =>{
 api.get('/details',(req,res) =>{
     res.status(200).json(data)
 })
-     
+
 api.listen(PORT, () => console.log('API running at ${HOST}:${PORT}'))
